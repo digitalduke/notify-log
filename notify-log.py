4907d944c5fe83dbec00fc715cd9429015a5500c
digitalduke <digitalduke@gmail.com>
2020-01-23 23:21:47 +0500

    add draft version of the notifications eavesdropper

---
+++ notify-log.py
@@ -0,0 +1,21 @@
+import dbus
+
+from gi.repository import GLib
+from dbus.mainloop.glib import DBusGMainLoop
+
+
+def process_notification(session_bus, message):
+    if message.get_member() == "Notify":
+        for arg in message.get_args_list():
+            print(arg)
+
+
+if __name__ == '__main__':
+    DBusGMainLoop(set_as_default=True)
+
+    bus = dbus.SessionBus()
+    bus.add_match_string_non_blocking("eavesdrop=true, interface='org.freedesktop.Notifications'")
+    bus.add_message_filter(process_notification)
+
+    mainloop = GLib.MainLoop()
+    mainloop.run()
