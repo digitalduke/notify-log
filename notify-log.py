a9659562b0d8d850c35a69adc69393ef5878e656
digitalduke <digitalduke@gmail.com>
2020-01-23 23:40:56 +0500

    add docstring

--- notify-log.py
+++ notify-log.py
@@ -5,6 +5,22 @@ from dbus.mainloop.glib import DBusGMainLoop
 
 
 def process_notification(session_bus, signal_message):
+    """
+    Process message and print payload to stdout.
+
+        session_bus: SessionBus (current login message bus)
+        message: SignalMessage (which can be sent or received over a D-Bus)
+
+        Message arguments depend on a member. For the 'org.freedesktop.Notifications.Notify' there are:
+            String app_name
+            UInt32 replaces_id
+            String app_icon
+            String summary
+            String body
+            Array of [String] actions
+            Dict of {String, Variant} hints
+            Int32 timeout
+    """
     if signal_message.get_member() == "Notify":
         for arg in signal_message.get_args_list():
             print(arg)
