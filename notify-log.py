d088f506867ff176da99a025bf9b9e3cfecd37da
Egor Pirogov <e.pirogov@gph.cian.ru>
2020-01-24 12:47:11 +0500

    add application name into the output

--- notify-log.py
+++ notify-log.py
@@ -26,10 +26,11 @@ def process_notification(session_bus, signal_message):
         return
 
     message = signal_message.get_args_list()
+    app_title = message[0]
     message_title = message[3]
     message_body = message[4]
     current_time = f'{time.localtime().tm_hour}:{time.localtime().tm_min}'
-    print(f'---[ {message_title} at {current_time} ]---\r\n{message_body}\r\n')
+    print(f'---[ {app_title} at {current_time} ]---\r\n{message_title}\r\n{message_body}\r\n')
 
 
 if __name__ == '__main__':
